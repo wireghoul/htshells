281c58f824c01c135e96b77a398e162da970c2f1
Eldar Marcussen <wireghoul@justanotherhacker.com>
2012-01-24 10:27:10 +1100

    Stealth shell teaser

--- stsh.pl
+++ stsh.pl
@@ -2,7 +2,9 @@
 # Interface for the mod_php htaccess stealth shell
 # Written by Wireghoul - http://www.justanotherhacker.com
 
-
+use warnings;
+use strict;
+use MIME::Base64;
 
 sub usage {
     print "Usage: $0 url\nExample: $0 http://vuln.com/upload/favicon.ico\n";
