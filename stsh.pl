ccf4015382145636bbcc1e0ee44682d18301cbfa
Eldar Marcussen <wireghoul@justanotherhacker.com>
2012-01-24 10:25:00 +1100

    Teaser

---
+++ stsh.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+# Interface for the mod_php htaccess stealth shell
+# Written by Wireghoul - http://www.justanotherhacker.com
+
+
+
+sub usage {
+    print "Usage: $0 url\nExample: $0 http://vuln.com/upload/favicon.ico\n";
+}
